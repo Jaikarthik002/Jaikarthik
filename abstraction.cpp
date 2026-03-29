fcae59eb4b31ace92b0817993985a0e0df2df2f3
Jaikarthik <jaikarthik002@gmail.com>
2026-03-29 18:13:36 +0530

    Add files via upload

---
+++ abstraction.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Data
+{
+public:
+
+    int a=10;
+public :
+
+    void func()
+    {
+        cout<<"jaikarthik"<<endl;
+    }
+};
+
+int main()
+{
+    Data obj;
+    obj.func();
+    cout<<"a="<<obj.a;
+}
